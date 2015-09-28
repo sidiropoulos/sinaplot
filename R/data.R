56ee866e88ed39009284e4342b7b19d6be7d04c1
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-09-28 14:39:00 +0200

    Sample dataset. Used in vignettes and examples.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Expression data from 2095 AML/ALL and healthy bone marrow cells.
+#'
+#' @usage data(blood)
+#' @format A data frame with 2095 rows and 2
+#' columns (AML/ALL subtype, expression values).
+#'
+"blood"
