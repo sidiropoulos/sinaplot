ba45b03a55cfe5d24507be48e472ea8444dd82a7
Nikos Sidiropoulos <nikos.sidiro@gmail.com>
2015-09-30 16:42:27 +0200

    Data sources.

--- R/data.R
+++ R/data.R
@@ -4,4 +4,10 @@
 #' @format A data frame with 2095 rows and 2
 #' columns (AML/ALL subtype, expression values).
 #'
+#' @source \url{http://servers.binf.ku.dk/bloodspot/}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE13159}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE15434}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE61804}
+#' @source \url{http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE14468}
+#' @source \url{http://cancergenome.nih.gov/}
 "blood"
